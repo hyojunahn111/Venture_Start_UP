cca8e8779aa426fee7186d590e795cc301b3b3bd
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-03-08 12:53:25 +0900

    240308

---
+++ chap01-literal-and-variable-lecture-source/src/com/ohgiraffers/section01/literal/Application1.java
@@ -0,0 +1,42 @@
+package com.ohgiraffers.section01.literal;
+
+public class Application1 {
+    public static void main(String[] args) {
+     /*여러가지 값의 형태를 출력해보자
+     * 1. 숫자 형태의 값
+     * 1-1. 정수 형태의 값
+     * 1-2. 실수 형태의 값
+     * 2. 문자 형태의 값
+     * 3. 문자열 형태의 값
+     * 4. 논리 형태의 값
+     * */
+
+
+
+     /*1. 숫자 형태의 값
+     * * 1-1. 정수 형태의 값 출력*/
+        System.out.println(123);
+        /*1-2. 실수 형태의 값*/
+        System.out.println(1.23);
+//        2. 문자 형태의 값 출력
+        System.out.println('a');                //문자 형태의 값은 홀따움표(single-quatation)으로 감싸우어야한다
+//        System.out.println('ab');             //두 개 이상은 문자로 취급하지 않기 때문에 에러가 발생한다.
+//        System.out.println('');               // 아무 문자도 기록되지 않는 경우도 에러가 발생한다
+        System.out.println('1');                //숫자 값 이지만 홀따움표로 감싸져 있는 경우 문자 '1' 이라고 판단한다.
+//        3. 문자열 형태의 값 출력
+        System.out.println("안녕하세요");         //물자열은 문자 여러 개가 나열된 형태를 말하며 쌍따움표(double-quatation)으로 감싸주어야한다
+        System.out.println("123");              //정수 이지만 쌍따움표로 감싸져있기 때문에 문자열로 보아야 한다
+        System.out.println("");                 //아무 값도 없는 빈 쌍따움표도 문자열로 취급한다
+        System.out.println("a");                //한 개의 문자도 쌍따움표로 감싸면 문자열이다. (문자a와 다르다)
+
+//        4. 논리 형태의 값 출력
+        System.out.println(true);               //true혹은 false 값을 논리형이라고 한다
+        System.out.println(false);
+
+
+
+
+
+
+    }
+}

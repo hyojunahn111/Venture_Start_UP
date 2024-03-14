7ffdd4b68ff26844fde7cb72c460cd3c4f1f7ac7
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-03-14 10:40:06 +0900

    testForExample메소드 실행

---
+++ chap04-control-flow-lecture-source/src/com/ohgiraffers/section02/looping/Application.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.section02.looping;
+
+public class Application {
+    public static void main(String[] args) {
+
+        A_for a = new A_for();
+        /*단독 for문 흐름 확인용 메소드 호출*/
+//        a.testSimpleForStatement();
+
+        /*반복문을 사용하는 예제*/
+        a.testForExample1();
+    }
+}

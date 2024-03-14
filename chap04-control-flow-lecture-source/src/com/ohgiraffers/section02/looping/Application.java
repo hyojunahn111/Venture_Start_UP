ef2d9d22933488339f801473a6f4f50a636b8293
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-03-14 14:17:19 +0900

    for문 실행 main

--- chap04-control-flow-lecture-source/src/com/ohgiraffers/section02/looping/Application.java
+++ chap04-control-flow-lecture-source/src/com/ohgiraffers/section02/looping/Application.java
@@ -10,6 +10,38 @@ public class Application {
         /*반복문을 사용하는 예제*/
 //        a.testForExample1();
 
-        a.testForExample2();
+//        a.testForExample2();
+
+//        a.testForExample3();
+
+//        a.testForExample4();
+
+        /*반복문을 일상 생활에서 사용하는 예제*/
+//        a.printSimpleGugudan();
+
+        A_nestedFor an = new A_nestedFor();
+        /*중첩 반복문을 사용하는 예제*/
+//        an.printGugudanFromTwoToNine();
+
+//        an.printGugudanFromTwoToNine();
+
+//        an.printStarInputRowTimes();
+
+//        an.printTriangleStars();
+
+        B_while bw = new B_while();
+
+//        bw.testSimpleWhileStatement();
+
+//        bw.testWhileExample1();
+
+//        bw.testWhileExample2();
+//        bw.testWhileExample3();
+
+        C_doWhile cd = new C_doWhile();
+
+//        cd.testSimpleDoWhileStatement();
+
+        cd.testDoWhileExample1();
     }
 }

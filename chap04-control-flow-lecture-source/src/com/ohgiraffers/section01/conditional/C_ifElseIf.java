8a41d31c3dc3474db65857754aa628af57221338
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-03-14 09:41:09 +0900

    c에 testNestedIfEsleIfStatement 메소드 추가

--- chap04-control-flow-lecture-source/src/com/ohgiraffers/section01/conditional/C_ifElseIf.java
+++ chap04-control-flow-lecture-source/src/com/ohgiraffers/section01/conditional/C_ifElseIf.java
@@ -50,4 +50,75 @@ public class C_ifElseIf {
         /*대답 여부와 상관 없이 마지막에 실행하는 내용*/
         System.out.println("산실령 사라짐");
     }
+
+    public void testNestedIfElseIfStatement(){
+
+        /*중첩된 if-else문의 흐름을 이해하고 적용할 수 있다*/
+        /*중첩된 if-else-if문 실행 흐름 확인*/
+        /*if-else-if문 안에서 또 다른 조건을 사용하여 if or if-else or if-else-if문을 사용할 수 있다*/
+
+        /*ohgiraffers 대학의 김ㅇㅇ교수는 학생들 시험 성적을 수기로 계산해서 학점 등급을 매기는 채점방식을 사용하고 있었다
+        * 90점 이상이면 'A', 80점 이상히면 'B', 70점 이상이면 'C', 60점 이상이면 'D'를
+        * 60점 미만인 경우에는 'F'를 학점 등급으로 하는 기준이다*/
+        /*추가로 각 등급의 중간점수(95, 85, 75...)인 경우 '+'를 붙여서 등급을 세분화 하다보니 신경쓸게 이만 저만이 아니다.
+        * 그래서 학생의 이름과 점수를 입력하면 자동으로 학점 등급이 계산되는 프로그램을 만들기로 했다*/
+
+        /*학생의 이름과 점수 입력
+        * */
+        Scanner sc = new Scanner(System.in);
+        System.out.println("학생의 이름을 입력하세요 : ");
+        String name = sc.nextLine();
+        System.out.println("학생의 점수를 입력하세요 : ");
+        int point =sc.nextInt();
+
+        /*학생의 등급을 저장하기 위한 변수 초기화*/
+        String grade = "";
+
+        /*학점 등급을 매기기 위해 점수를 확인하기 위한 조건문*/
+        if (point >= 90){
+
+            /*점수가 90점이 이상인 경우*/
+            grade = "A";
+
+            /*등급은 A등급인데 그럼 +가 붙을 자격이 있는(90점 이상인자) 다시 확인*/
+            if(point >= 95){
+
+                /*95점 이상인 경우 A등급에 +기호를 덧붙여 A+등급을 만들어준다*/
+                grade += "+";
+            }
+
+        }else if(point >= 80){
+
+            /*점수가 90점 미만, 80점 이상인 경우*/
+            grade = "B";
+
+            if(point >= 85){
+                grade += "+";
+            }
+
+        }else if(point >= 70) {
+
+            grade = "C";
+
+            if (point >= 75) {
+
+                grade += "+";
+
+            }
+        }else if(point >= 60){
+
+            grade = "D";
+
+            if(point >= 65){
+                grade += "+";
+            }
+
+        }else{
+            grade = "F";
+        }
+
+        System.out.println(name + "학생의 점수는"  + point + "이고, 등급은" + grade + "입니다.");
+        System.out.println("프르그램을 종료합니다.");
+
+    }
 }

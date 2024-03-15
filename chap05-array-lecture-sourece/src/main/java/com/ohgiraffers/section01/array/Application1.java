14aa4f3e480a231bd84c0c45f982633c073552f6
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-03-15 09:16:09 +0900

    continue문 study1

---
+++ chap05-array-lecture-sourece/src/main/java/com/ohgiraffers/section01/array/Application1.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.section01.array;
+
+public class Application1 {
+    public static void main(String[] args) {
+        /*배열의 대해 이행하고 배열의 사용 목적을 이해할 수 있다*/
+        /*배열이란?*/
+        /*동일한 자료형의 묶음(연속된 메모리 공간에*/
+    }
+}

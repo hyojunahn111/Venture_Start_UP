4c1bac4300bcbabe8bb8337e8de89ac00002d38c
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-03-20 09:54:18 +0900

    class study6

---
+++ chap06-class-and-object/src/main/java/com/ohgiraffers/section02/encapuslation/problem2/Monster.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.section02.encapuslation.problem2;
+
+public class Monster {
+
+//    String name;
+//    int hp;
+//
+
+    /*2. name 필드의 이름을 kinds 변경한다*/
+    /*몬스터의 이름이 아니고 종류가 해당하는 내용을 입력한다*/
+
+    String kind;
+    int hp;
+
+}

e767fb52a11c474636bc1333228e12fa49fd53fe
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-03-22 13:58:48 +0900

    객체배열의 이해4(회원가입프로그램)

---
+++ chap07-object-array-lecture-source/src/main/java/com/ohgiraffers/section02/uses/Member.java
@@ -0,0 +1,40 @@
+package com.ohgiraffers.section02.uses;
+
+public class Member {
+
+    private int num;
+    private String id;
+    private String pwd;
+    private String name;
+    private int age;
+    private char gender;
+
+
+    public Member(){}
+
+    public Member(int num, String id, String pwd, String name, int age, char gender){
+        this.num = num;
+        this.id = id;
+        this.pwd = pwd;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public void setNum(int num){this.num = num;}
+    public void setid(String id){this.id = id;}
+    public void setName(String name){this.name = name;}
+    public void setAge(int age){this.age = age;}
+    public void setGender(char gender){this.gender = gender;}
+
+    public int getNum(){return num;}
+    public String getId(){return id;}
+    public String getName(){return name;}
+    public int getAge(){return age;}
+    public char getGender(){return gender;}
+
+
+    public String getInformation(){
+        return "Member [num=" + num + ", id" + id + ", pwd" + pwd + ", name" + name + ", age" + age + ", gender" + gender + "]";
+    }
+}

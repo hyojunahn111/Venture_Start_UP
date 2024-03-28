4525e7219e0b2cafe6fc7ac02b9794a16820967e
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-03-28 15:29:04 +0900

    귀찮아서 다 커밋안하고 푸쉬

---
+++ java/chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section02/abstractclass/SmartPhone.java
@@ -0,0 +1,12 @@
+package com.ohgiraffers.section02.abstractclass;
+
+/* 설명. 두 개 이상의 클래스를 extends로 상속받으면 컴파일 에러가 발생한다. */
+public class SmartPhone /*, java.util.Scanner */ {
+
+	/* 설명.
+	 *  SmartPhone 클래스는 Product클래스를 상속받아서 구현한다.
+	 *  extends 키워드로 클래스를 상속받을 때 두 개 이상의 클래스는 상속하지 못한다. (모호성 제거)
+	 *  추상클래스가 가지는 추상 메소드를 반드시 오버라이딩 해야 한다. (강제성 부여)
+	 * */
+
+}
---
+++ java/chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section03/interfaceimplements/InterProduct.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section03.interfaceimplements;
+
+public interface InterProduct extends java.io.Serializable/*, java.util.Comparator*/ {
+
+	/* 필기.
+	 *  인터페이스가 인터페이스를 상속받을 시에는 extends 키워드를 이용하며
+	 *  이 때는 여러 인터페이스를 다중 상속 받을 수 있다.
+	 * */
+	
+	/* 필기.
+	 *  인터페이스는 상수 필드만 작성 가능하다.
+	 *  public static final 제어자 조합을 상수 필드라고 부른다.
+	 *  반드시 선언과 동시에 초기화 해줘야 한다.
+	 * */
+
+}

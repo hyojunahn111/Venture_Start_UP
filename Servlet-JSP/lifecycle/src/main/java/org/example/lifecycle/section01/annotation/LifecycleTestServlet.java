4946404ccb7f56d61f31d3b9f928d7ed5d1250b1
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-05-07 11:10:40 +0900

    React async api - fetch async await

--- Servlet-JSP/lifecycle/src/main/java/org/example/lifecycle/section01/annotation/LifecycleTestServlet.java
+++ Servlet-JSP/lifecycle/src/main/java/org/example/lifecycle/section01/annotation/LifecycleTestServlet.java
@@ -8,13 +8,39 @@ import java.io.IOException;
 
 @WebServlet(value = "/annotation-lifecycle")
 public class LifecycleTestServlet extends HttpServlet {
+
+    private int initCount = 1;
+
+    private int serviceCount = 1;
+
+    private int destroyCount = 1;
+
+    public LifecycleTestServlet() {
+        System.out.println("기본 생성자 호출됨");
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("annotation 매핑 init() 메소드 호출 : " + initCount++);
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("annotation 매핑 service() 메소드 호출 : " + serviceCount++);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("annotation 매핑 destroy() 메소드 호출 : " + destroyCount++);
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        System.out.println("annotation 매핑 doGet() 메소드 호출");
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        System.out.println("annotation 매핑 doPost() 메소드 호출");
     }
 }

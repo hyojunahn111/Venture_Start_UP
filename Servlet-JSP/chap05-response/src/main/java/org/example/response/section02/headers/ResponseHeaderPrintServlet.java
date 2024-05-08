40763d5843b70e035ee2b4925017f88b45f851a8
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-05-08 09:49:55 +0900

    Servlet chap02 ServiceMethodTestServlet

---
+++ Servlet-JSP/chap05-response/src/main/java/org/example/response/section02/headers/ResponseHeaderPrintServlet.java
@@ -0,0 +1,20 @@
+package org.example.response.section02.headers;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+import java.io.IOException;
+
+@WebServlet("/headers")
+public class ResponseHeaderPrintServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}

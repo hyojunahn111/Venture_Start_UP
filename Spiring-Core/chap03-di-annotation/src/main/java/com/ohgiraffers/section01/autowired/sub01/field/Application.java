ece6730869a8dddc0ed79cb67962b10d47ccb5d5
hyojunahn111 <59754350+hyojunahn111@users.noreply.github.com>
2024-05-13 09:23:46 +0900

    chap01-ioc-container section02 Application

--- Spiring-Core/chap03-di-annotation/src/main/java/com/ohgiraffers/section01/autowired/sub01/field/Application.java
+++ Spiring-Core/chap03-di-annotation/src/main/java/com/ohgiraffers/section01/autowired/sub01/field/Application.java
@@ -2,6 +2,6 @@ package com.ohgiraffers.section01.autowired.sub01.field;
 
 public class Application {
     public static void main(String[] args) {
-        
+
     }
 }
---
+++ Spiring-Core/chap03-di-annotation/src/main/java/com/ohgiraffers/section01/common/BookDTO.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.section01.common;
+
+import java.util.Date;
+
+public class BookDTO {
+
+    private int sequence;
+    private int isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private Date createdDate;
+    
+}
